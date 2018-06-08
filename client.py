955f6f6490c9b01f8fe9d269dc5f4e3ec70c44b6
happiness4ever <39913100+happiness4ever@users.noreply.github.com>
2018-06-08 12:06:14 +0000

    Create client.py

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+def socket_client():
+    try:
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.connect(('127.0.0.1',6666))
+    except socket.error as msg:
+	print msg
+	sys.exit(1)
+    print s.recv(1024)
+    while 1:
+	data=raw_input('please input work:')
+	s.send(data)
+	print s.recv(1024)
+	if data=='exit':
+	    break
+    s.close()
+
+
+if __name__=='__main__':
+    socket_client()
